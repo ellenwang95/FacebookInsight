734c94fdd74397853f8dc2a23da59e85c9efdbf9
Ellen Wang <ellenwang95@yahoo.ca>
2016-07-15 02:24:43 -0700

    added some data to test

--- input_train_data.py
+++ input_train_data.py
@@ -1,9 +1,10 @@
 from textblob import TextBlob
 
-classification_types = ['entertainment', 'food', 'art', 'funny/cute', 'psychology', 'news', 'misc', 'friend_post']
+classification_types = ['entertainment', 'food', 'art', 'funny/cute', 'psychology', 'news', 'misc', 'friend_post', 'group_post']
+media_types = ['vid', 'img', 'txt', 'link']
 
 def validate_media_type(media_type_input):
-	return media_type_input in ['vid', 'img', 'txt']
+	return media_type_input in media_types
 
 def validate_reaction_count(reaction_count_input):
 	return reaction_count_input.isdigit()
@@ -16,7 +17,8 @@ def validate_classification(classification_input):
 user_content = raw_input('Enter the User Content: ') #text input expected 
 media_type = raw_input('Enter the Media Type: ') #vid for video, img for image, txt for plain text
 while not validate_media_type(media_type):
-	print('ERROR: The valid media types are: vid | img | txt. Pls try again.')
+	print('ERROR: The valid media types are: ')
+	print(media_types)
 	media_type = raw_input('Enter the Media Type: ') 
 reaction_count = raw_input('Enter the Reaction Count: ') #integer input expected 	
 while not validate_reaction_count(reaction_count):
@@ -34,8 +36,11 @@ print('Media Type: %s') % media_type
 print('Reaction Count: %s') % reaction_count
 print('Classification: %s') % classification
 
-with open('train.csv', 'a') as f:
-	f.write(user_content + ',' + classification + '\n') # python will convert \n to os.linesep
+with open('data.csv', 'a') as data_f: 
+	data_f.write(user_content + ',' + media_type + ',' + reaction_count + ',' + classification + '\n') # python will convert \n to os.linesep
+
+with open('train.csv', 'a') as train_f:
+	train_f.write(user_content + ',' + classification + '\n') # python will convert \n to os.linesep
 
 
 
